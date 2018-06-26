37147e16cc683831f8f1ca2d28f9408b1507c650
Ralf Baussenwein <r.baussenwein@eikona-media.de>
2018-06-26 19:00:02 +0200

    workaround for sql_mode=only_full_group_by https://github.com/terminal42/contao-leads/issues/49

--- src/DataCollector.php
+++ src/DataCollector.php
@@ -202,7 +202,7 @@ class DataCollector
 
         $data = [];
         $db = \Database::getInstance()->prepare("
-            SELECT * FROM (
+            SELECT id, ANY_VALUE(name), ANY_VALUE(label), type, options, field_id, ANY_VALUE(master_id), ANY_VALUE(sorting) FROM (
                 SELECT
                     tl_lead_data.field_id AS id,
                     IFNULL(tl_form_field.name, tl_lead_data.name) AS name,
